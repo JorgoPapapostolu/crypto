75059ce5efafcc837ee82b19e5ff52fa06b85985
JorgoPapapostolu <jorgopapapostolu@gmail.com>
2024-03-06 18:21:45 +0100

    base

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,13 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
+	import logo from '$lib/images/bitcoin-btc-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
 	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
+		<a href="https://pro.kraken.com/">
+			<img src={logo} alt="BTC" />
 		</a>
 	</div>
 
@@ -17,13 +17,13 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/home">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname.startsWith('/tracker') ? 'page' : undefined}>
+				<a href="/tracker">Tracker</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -32,7 +32,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/JorgoPapapostolu">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
@@ -114,7 +114,7 @@
 		height: 100%;
 		align-items: center;
 		padding: 0 0.5rem;
-		color: var(--color-text);
+		color: black;
 		font-weight: 700;
 		font-size: 0.8rem;
 		text-transform: uppercase;
